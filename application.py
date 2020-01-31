4e73831d8a0f764ce7a049610f9ebeccc07945c9
Christian Benade <christian.benade@live.ca>
2020-01-31 13:28:03 -0800

    Final fix to login/logout issue (hopefully)

--- application.py
+++ application.py
@@ -47,10 +47,14 @@ def index():
 ##################################################################################################################
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if session["username"] is not None:
+    try:
         if session["username"] in users:
             users.remove(session["username"])
+    except:
+        pass
+    finally:
         session.clear()
+
     if request.method == "POST":
         username = request.form.get("username")
         if username not in users:
